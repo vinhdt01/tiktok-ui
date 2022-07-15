576bc71d4ffc2a3705a79399a4ab5c4881216f1d
daothevinh123 <daothevinh.tv@gmail.com>
2022-07-15 19:57:08 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok #6

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -8,18 +8,29 @@ import {
     faEllipsisVertical,
     faMagnifyingGlass,
     faKeyboard,
+    faCloudUpload,
+    faMessage,
+    faCoins,
+    faGear,
+    faSignOut,
 } from '@fortawesome/free-solid-svg-icons';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import AccountItem from '~/components/AccountItem';
 import styles from './Header.module.scss';
 import images from '~/asses/images';
 import Button from '~/components/Button/index';
-import Tippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react';
+
+import HeadlessTippy from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
+
 import Menu from '~/components/Popper/Menu/index';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 function Header() {
     const cx = classNames.bind(styles);
     const [searchResult, setSearchResult] = useState([]);
+    const currentUser = true;
+
     const MENU_ITEMS = [
         {
             icon: <FontAwesomeIcon icon={faEarthAsia} />,
@@ -63,11 +74,35 @@ function Header() {
             default:
         }
     };
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+            title: 'View profile',
+            to: '/hoaa',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get coins',
+            to: '/coins',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignOut} />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="logo" />
-                <Tippy
+                <HeadlessTippy
                     visible={searchResult.length > 0}
                     interactive
                     render={(attrs) => (
@@ -93,14 +128,34 @@ function Header() {
                             </span>
                         </button>
                     </div>
-                </Tippy>
-                <div className={cx('action')}>
-                    <Button text>Upload</Button>
-                    <Button primary>Log in</Button>
-                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
-                        <button className={cx('more-btn')}>
-                            <FontAwesomeIcon icon={faEllipsisVertical} />
-                        </button>
+                </HeadlessTippy>
+                <div className={cx('actions')}>
+                    {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <FontAwesomeIcon icon={faCloudUpload} />
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary>Log in</Button>
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <img
+                                className={cx('user-avatar')}
+                                alt="user-avatar"
+                                src="https://kenh14cdn.com/thumb_w/660/2020/9/12/whynotchao508262882833455156913728299053622333116833n-1599900492232643493066.jpg"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
                     </Menu>
                 </div>
             </div>
--- src/components/Popper/Menu/MenuItem.js
+++ src/components/Popper/Menu/MenuItem.js
@@ -6,9 +6,12 @@ import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 function MenuItem({ data, onClick }) {
     const cx = classNames.bind(styles);
+    const classes = cx('menu-item', {
+        separate: data.separate,
+    });
 
     return (
-        <Button leftIcon={data.icon} to={data.to} className={cx('memu-item')} onClick={onClick}>
+        <Button leftIcon={data.icon} to={data.to} className={classes} onClick={onClick}>
             {data.title}
         </Button>
     );
